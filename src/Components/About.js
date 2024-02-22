05c9088a57252fbbd93604bb60f37805ac6825cd
Joshi Dev Jitenkumar <devjjoshi0910@gmail.com>
2024-02-22 10:06:25 +0530

    Added Image in about us and some data for the same

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,12 +7,19 @@ const About = () => {
   return (
     <div className="about-container">
       <div className="image-holder">
-        <img src="Rashmi_Vadgama.jpg" alt="Rashmi's AACHAR HOUSE" />
+        <img src="Rashmi_Vadgama.jpg" alt="Rashmi Vadgama" />
       </div>
       <div className="text-content">
-        <h2>VADGAMA RASHMI</h2>
+        
         <p>
-          Welcome to Rashmi's AACHAR HOUSE, where I specialize in crafting delicious homemade pickles with love and care. My journey started with a passion for preserving traditional flavors and providing a unique taste experience to people.
+        Crafting Tradition, One Jar at a Time:<br></br> The Story of <strong>Vadgama Rashmi's</strong> Homemade Pickles<br></br>
+
+Ever wondered how a childhood observation could turn into a recipe for success? Meet Rashmi Vadgama, the creative force behind our delectable homemade pickles.
+From the tender age of 14, rashmi found herself captivated by the artistry of pickle-making in her mother's kitchen. Year after year, she soaked in the aromatic symphony of spices and the dance of ingredients, laying the foundation for a lifelong passion.
+Inspired by the cherished flavors passed down through generations, rashmi embarked on a journey to bring these timeless tastes to a broader audience. The magic? The rich tapestry of flavors that found their way into every jar, a legacy of love from her mother's kitchen to yours.
+Starting small, rashmi began sharing her creations with neighbors and relatives. The response was nothing short of impressive! The unmistakable burst of flavors in each jar left people craving more, and that's when the seeds of a homemade pickle empire were planted.
+Fast forward to today, and rashmi's Homemade Pickles has become a household name. Our jars are filled not just with pickles but with the warmth of tradition, the love of family, and the essence of culinary artistry.
+Join us on this savory journey as we continue to craft memories, one jar at a time!❤️
         </p>
         {/* Add more details or customize the description as needed */}
       </div>
